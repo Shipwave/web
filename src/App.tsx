19016c96950ed2e7e09ce8b80f79c2c8a4e6e536
campbell-frost <campbellsfrost@gmail.com>
2024-08-23 00:06:11 -0400

    Updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createSignal, type Component } from 'solid-js';
-
 import styles from './App.module.css';
 import Navbar from './components/Navbar';
 
@@ -14,36 +13,53 @@ interface Song {
 const App: Component = () => {
   const [song, setSong] = createSignal<Song | null>(null);
   const [error, setError] = createSignal<string[]>(["", ""]);
-
+  const [audioUrl, setAudioUrl] = createSignal<string | null>(null);
 
   const getSong = async () => {
     try {
       const response = await fetch("http://localhost:3000/song/1")
       const json = await response.json()
-      console.log(response, json)
       setSong(json);
+      playAudio(song()!.audio_bytes)
     } catch (e: any) {
       if (e instanceof Error)
         setError([e.message, e.name])
     }
   }
 
+  const playAudio = (base64Audio: string) => {
+    const byteCharacters = atob(base64Audio);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: 'audio/mp3' });
+    const url = URL.createObjectURL(blob);
+    setAudioUrl(url);
+  };
+
+
   return (
     <>
       <Navbar />
-      <button class={styles.button} onClick={getSong}>Fetchsong from "/"</button>
-      <button onClick={getSong}>Get song</button>
-      {song() &&
-        <div>
-          <h1>{song()!.title}</h1>
-          <p>Artist: {song()!.artist}</p>
-          <p>Album: {song()!.album}</p>
+      <div class={styles.container}>
+        <div class={styles.content}>
+          <button class={styles.getSong} onClick={getSong}>Get song</button>
+          {song() && (
+            <div class={styles.info}>
+              <h1 class={styles.title}>{song()!.title}</h1>
+              <p class={styles.detail}>Artist: {song()!.artist}</p>
+              <p class={styles.detail}>Album: {song()!.album}</p>
+              {audioUrl() && <audio class={styles.player} src={audioUrl()!} controls />}
+            </div>
+          )}
+          {error()[0] && (
+            <p class={styles.error}>An error occurred: {error()[0]} - {error()[1]}</p>
+          )}
         </div>
-      }
+      </div>
 
-      {error()[0] && (
-        <p>An error occurred: {error()[0]} - {error()[1]}</p>
-      )}
     </>
   );
 };
