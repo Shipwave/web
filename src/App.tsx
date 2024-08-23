db32cbb64573d0271c2556ef71ceec704694ffb0
campbell-frost <campbellsfrost@gmail.com>
2024-08-22 23:35:11 -0400

    getting data from getSong endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,47 @@ import { createSignal, type Component } from 'solid-js';
 import styles from './App.module.css';
 import Navbar from './components/Navbar';
 
-interface JsonRespose {
-  body: string;
+interface Song {
+  id: number;
+  title: string;
+  artist: string;
+  album: string;
+  audio_bytes: string;
 }
 
 const App: Component = () => {
-  const [data, setData] = createSignal<JsonRespose | null>(null);
-  const [error, setError] = createSignal<string>("");
-  const fetchData = async () => {
+  const [song, setSong] = createSignal<Song | null>(null);
+  const [error, setError] = createSignal<string[]>(["", ""]);
+
+
+  const getSong = async () => {
     try {
-      const response = await fetch("http://localhost:3000/");
-      const json = await response.json();
-      setData(json);
+      const response = await fetch("http://localhost:3000/song/1")
+      const json = await response.json()
+      console.log(response, json)
+      setSong(json);
     } catch (e: any) {
-      if (e instanceof Error) {
-        setError(e.message);
-      } else {
-        setError(e);
-      }
-      console.log(e);
+      if (e instanceof Error)
+        setError([e.message, e.name])
     }
   }
 
   return (
     <>
       <Navbar />
-      <button class={styles.button} onClick={fetchData}>FetchData from "/"</button>
-      {data()  &&
-        <p>Response data: {data()!.body}</p>
+      <button class={styles.button} onClick={getSong}>Fetchsong from "/"</button>
+      <button onClick={getSong}>Get song</button>
+      {song() &&
+        <div>
+          <h1>{song()!.title}</h1>
+          <p>Artist: {song()!.artist}</p>
+          <p>Album: {song()!.album}</p>
+        </div>
       }
-      {error() &&
-        <p>An error occured : {error()}</p>
-      }
+
+      {error()[0] && (
+        <p>An error occurred: {error()[0]} - {error()[1]}</p>
+      )}
     </>
   );
 };
