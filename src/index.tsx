bdf315cd87c9984b824e78aced756250bfa00e35
campbell-frost <campbellsfrost@gmail.com>
2024-08-25 14:59:21 -0400

    Added routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-
+import { Route, Router } from '@solidjs/router';
 import './index.css';
 import App from './App';
 
@@ -12,4 +12,10 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   );
 }
 
-render(() => <App />, root!);
+render(
+  () => (
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  ), root!
+);
