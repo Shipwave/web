2cf7a29b5759586fbd60b046f0e0d90b2841697c
campbell-frost <campbellsfrost@gmail.com>
2024-08-25 14:51:17 -0400

    Refactored content

---
+++ src/components/Content.tsx
@@ -0,0 +1,63 @@
+import { Component, createSignal } from "solid-js";
+import styles from '../styles/Content.module.css';
+
+interface Song {
+  id: number;
+  title: string;
+  artist: string;
+  album: string;
+  audio_bytes: string;
+}
+
+const Content: Component = () => {
+  const [song, setSong] = createSignal<Song | null>(null);
+  const [error, setError] = createSignal<string[]>(["", ""]);
+  const [audioUrl, setAudioUrl] = createSignal<string | null>(null);
+
+  const getSong = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/song/1")
+      const json = await response.json()
+      setSong(json);
+      playAudio(song()!.audio_bytes)
+    } catch (e: any) {
+      if (e instanceof Error)
+        setError([e.message, e.name])
+    }
+  }
+
+  const playAudio = (base64Audio: string) => {
+    const byteCharacters = atob(base64Audio);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: 'audio/mp3' });
+    const url = URL.createObjectURL(blob);
+    setAudioUrl(url);
+  };
+
+  return (
+    <div class={styles.container}>
+        <div class={styles.content}>
+          <button class={styles.getSong} onClick={getSong}>Get song</button>
+          {song() && (
+            <div class={styles.info}>
+              <h1 class={styles.title}>{song()!.title}</h1>
+              <p class={styles.detail}>Artist: {song()!.artist}</p>
+              <p class={styles.detail}>Album: {song()!.album}</p>
+              {audioUrl() && <audio class={styles.player} src={audioUrl()!} controls />}
+            </div>
+          )}
+          {error()[0] && (
+            <p class={styles.error}>An error occurred: {error()[0]} - {error()[1]}</p>
+          )}
+        </div>
+      </div>
+
+  )
+
+}
+
+export default Content;
